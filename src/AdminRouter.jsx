cc2cd5a111b8f37f5e3e722ef78dd1bad6512989
Raahim Fareed <raahim1965@hotmail.com>
2023-08-18 11:54:50 +0500

    created states page and integrated api into it

--- src/AdminRouter.jsx
+++ src/AdminRouter.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import AddQuestion from "./Pages/AddQuestion";
+import AddState from "./Pages/AddState";
 
 const AdminRouter = () => {
   return (
     <Routes>
       <Route path="/question/add" element={<AddQuestion />} />
+      <Route path="/states/add" element={<AddState />} />
     </Routes>
   );
 };
